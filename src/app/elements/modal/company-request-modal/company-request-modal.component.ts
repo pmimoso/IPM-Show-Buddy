7cf53473dc683da01149e081a71730bd8de211fa
jf-barbosa <jf.barbosa@campus.fct.unl.pt>
2019-11-19 21:18:15 +0000

    Another autocomplete done.

--- src/app/elements/modal/company-request-modal/company-request-modal.component.ts
+++ src/app/elements/modal/company-request-modal/company-request-modal.component.ts
@@ -4,6 +4,9 @@ import { FormControl } from '@angular/forms';
 import { RequestsService } from 'src/app/services/requests.service';
 import { LoggedUserService } from 'src/app/services/logged-user.service';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
+import { EventsService } from 'src/app/services/events.service';
 
 @Component({
   selector: 'app-company-request-modal',
@@ -13,12 +16,29 @@ import { Router } from '@angular/router';
 export class CompanyRequestModalComponent implements OnInit {
 
   sex = new FormControl();
+  showCtrl = new FormControl();
+  showList: Show[] = [];
+  filteredShows: Observable<Show[]>;
   sexList: string[] = ['Feminino', 'Masculino', 'Outro'];
 
   constructor(public activeModal: NgbActiveModal, private requestService: RequestsService, 
-    private loggedUserService: LoggedUserService, private route: Router) { }
+    private loggedUserService: LoggedUserService, private route: Router, 
+    private eventsService: EventsService) {
+      this.filteredShows = this.showCtrl.valueChanges
+      .pipe(
+        startWith('') ,
+        map(state => state ? this._filterShows(state) : this.showList.slice())
+      );
+     }
 
   ngOnInit() {
+    this.showList = this.eventsService.getAllShows();
+  }
+
+  private _filterShows(value: string): Show[] {
+    const filterValue = value.toLowerCase();
+
+    return this.showList.filter(state => state.name.toLowerCase().indexOf(filterValue) === 0);
   }
 
   closeModal() {
